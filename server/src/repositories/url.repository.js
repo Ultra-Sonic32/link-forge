cbe7b5fda32abf10eca5fbd44472bec5c6d38d4d
Ultra-Sonic32 <simonwesthof@outlook.com>
2025-08-20 10:01:03 +0200

    add url repository methods

---
+++ server/src/repositories/url.repository.js
@@ -0,0 +1,60 @@
+import Url from '../models/url';
+
+/**
+ * Save Url Data to the database
+ * @param {*} data
+ * @returns
+ */
+const saveUrl = async data => {
+  const url = new Url(data);
+  return await url.save();
+};
+
+/**
+ * Find specific record by shortUrl
+ * @param {string} shortUrl
+ * @returns
+ */
+const findByShortUrl = async shortUrl => {
+  return Url.findOne({ shortUrl, isActive: true });
+};
+
+/**
+ * Find specific record by snowflake ID
+ * @param {string} snowflakeId
+ * @returns
+ */
+const findBySnowID = async snowflakeId => {
+  return Url.findOne({ snowflakeId });
+};
+
+/**
+ * Check if shortKey exist return true or false
+ * @param {string} shortUrl
+ * @returns
+ */
+const isShortKeyTaken = async shortUrl => {
+  const existing = await Url.findOne({ shortUrl });
+  return existing ? true : false;
+};
+
+/**
+ * Increment the clicks field for each shortend url
+ * @param {string} shortUrl
+ * @returns
+ */
+const incrementClicks = async shortUrl => {
+  return await Url.findOneAndUpdate(
+    { shortUrl },
+    {
+      $inc: { clicks: 1 },
+      $set: { lastAccessed: new Date() }
+    },
+    { new: true }
+  );
+};
+
+//TODO Add Logic in here to return all record with maybe extrat parms to filter like is active or by some date
+const findAllShortUrl = async () => {};
+
+export { saveUrl, findByShortUrl, findBySnowID, isShortKeyTaken, findAllShortUrl, incrementClicks };
