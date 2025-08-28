3db6da9da60616b97c4ccb3386ff524eb04bf77a
Ultra-Sonic32 <simonwesthof@outlook.com>
2025-08-28 23:58:16 +0200

    Update the navbar so it can hide on scroll

--- link-forge-client/src/app/components/navbar/navbar.component.ts
+++ link-forge-client/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -9,4 +9,21 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isVisible = true;
+  lastScrollTop = 0;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Hide on scroll down, show on scroll up or top
+    if (scrollTop > this.lastScrollTop && scrollTop > 100) {
+      this.isVisible = false;
+    } else {
+      this.isVisible = true;
+    }
+
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  }
+}
